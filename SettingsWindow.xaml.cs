aa5817725e1c3fa62d7355a0b0995cd986a31b5d
Echostorm <echostorm@outlook.com>
2024-06-21 16:12:50 -0400

    Settings, System Tray icon, better hotkey focus and way to change hotkey and much much more
    Enhanced app functionality and UI updates
    
    - App.xaml: Added an empty line for clarity.
    - App.xaml.cs: Introduced file operations, path management, and directory initialization.
    - ClipClop.csproj: Included new images and a package for system tray icon functionality.
    - Helpers.cs: Added image processing methods and updated path references.
    - HotKey.cs: Improved memory management by removing disposed hotkeys.
    - MainWindow.xaml: Enhanced UI with taskbar settings and key bindings.
    - MainWindow.xaml.cs: Added JSON serialization for data management and new commands for UI interaction.
    - SettingsWindow: Created a new window for hotkey configuration.
    - Added `Close.png` for UI enhancement.

---
+++ SettingsWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ClipClop;
+
+/// <summary>
+/// Interaction logic for SettingsWindow.xaml
+/// </summary>
+public partial class SettingsWindow : Window
+{
+    MainWindow parent;
+    bool initializing = true;
+    public ICommand EscapeCommand { get; set; }
+
+    public SettingsWindow(MainWindow main)
+    {
+        this.EscapeCommand = new RelayCommand((a) => EscapeButtonClick());
+        InitializeComponent();
+        this.DataContext = this;
+        parent = main;
+    }
+
+    private void Close_Click(object sender, RoutedEventArgs e)
+    {
+        this.Close();
+    }
+
+    void EscapeButtonClick()
+    {
+        this.Close();
+    }
+
+    private void Window_SourceInitialized(object sender, EventArgs e)
+    {
+        initializing = true;
+        var keyList = Enum.GetValues(typeof(Key)).Cast<Key>().ToList();
+        ddlKey.ItemsSource = keyList;
+        var modList = Enum.GetValues(typeof(KeyModifier)).Cast<KeyModifier>().ToList();
+        ddlMod.ItemsSource = modList;
+        ddlKey.SelectedItem = parent.ShowHotKey.Key;
+        ddlMod.SelectedItem = parent.ShowHotKey.KeyModifiers;
+        initializing = false;
+    }
+
+    private void HotkeySelection_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if(initializing)
+        {
+            return;
+        }
+        if(ddlKey.SelectedItem != null && ddlKey.SelectedItem is Key && 
+            ddlMod.SelectedItem != null && ddlMod.SelectedItem is KeyModifier)
+        {
+            parent.SetNewHotkey((Key)ddlKey.SelectedItem, (KeyModifier)ddlMod.SelectedItem);
+        }
+    }
+}
